8edead9490efdcb9b83edacf539ae6c6c05f33b7
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-11 22:15:08 +0530

    Dificulty-513

---
+++ CODECHEF/JanmanshandAssignments_513.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,x;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++){
+        scanf("%d",&x);
+        if(x<=7){
+            printf("Yes\n");
+        }
+        else{
+            printf("No\n");
+        }
+    }
+    
+	
+
+}
+
