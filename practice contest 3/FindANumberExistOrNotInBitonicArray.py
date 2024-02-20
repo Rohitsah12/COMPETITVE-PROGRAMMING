3c5a3a9a077737ed039144ac18fdba15b7bf741e
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-20 19:18:25 +0530

    Not Being SOlved

---
+++ practice contest 3/FindANumberExistOrNotInBitonicArray.py
@@ -0,0 +1,18 @@
+def findMax(A):
+    low=0
+    high=len(A)-1
+    while(low<=high):
+        mid=(low+high)//2
+        if A[mid-1]<A[mid] and A[mid]>A[mid+1]:
+            return mid
+        elif A[mid-1]<A[mid] and A[mid]<A[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+    return mid
+def inc(findmax(A)):
+    low=0
+    high=findMax(A)
+    while(low<=high):
+        if(A[mid]==target):
+            
