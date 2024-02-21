be1649ea6e4d4ff2b495b1df34177e904f139876
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-21 19:54:44 +0530

    Practice

---
+++ codeforces/4A_Watermelon.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if(n>2){
+    if (n % 2 == 0) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+    }
+    else{
+        cout<<"No\n";
+    }
+    return 0;
+}
