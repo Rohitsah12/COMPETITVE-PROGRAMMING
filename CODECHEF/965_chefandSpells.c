dd44d4540e5388ec3d1d637e58deec731de31454
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-02-22 20:49:38 +0530

    965

---
+++ CODECHEF/965_chefandSpells.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	int a,b,c;
+	for(int i=0;i<t;i++){
+	    scanf("%d %d %d",&a,&b,&c);
+	    int x=a+b;
+	    int y=b+c;
+	    int z=c+a;
+	    if(x>y){
+	        if(x>z){
+	            printf("%d\n",x);
+	        }
+	        else{
+	            printf("%d\n",z);
+	        }
+	    }
+	    else{
+	        if(y>z){
+	            printf("%d\n",y);
+	        }
+	        else{
+	            printf("%d\n",z);
+	        }
+	    }
+	}
+
+}
+
