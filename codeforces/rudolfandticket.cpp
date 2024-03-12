28ae36315e93413b37c5f831712483de5a63199e
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-12 21:58:44 +0530

    rudolf and ticket

---
+++ codeforces/rudolfandticket.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+
+    for (int i = 0; i < t; ++i) {
+        int n, m, k;
+        cin >> n >> m >> k;
+
+        int leftCoins[n];
+        int rightCoins[m];
+        
+        for (int j = 0; j < n; ++j) {
+            cin >> leftCoins[j];
+        }
+        
+        for (int j = 0; j < m; ++j) {
+            cin >> rightCoins[j];
+        }
+        
+        int count = 0;
+        for (int x = 0; x < n; ++x) {
+            for (int y = 0; y < m; ++y) {
+                if (leftCoins[x] + rightCoins[y] <= k) {
+                    count++;
+                }
+            }
+        }
+        
+        cout << count << endl;
+    }
+
+    return 0;
+}
