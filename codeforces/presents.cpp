a2e424eb3af24df2f1493da66c89a818c6e4f7be
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-16 21:40:07 +0530

    presentes

--- codeforces/presents.cpp
+++ codeforces/presents.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 std::vector<int> find_gift_givers(int n, const std::vector<int>& gifts) {
-    std::vector<int> givers(n, 0);  // Initialize array to store gift givers
+    std::vector<int> givers(n, 0);
 
     for (int i = 0; i < n; ++i) {
         int friendIdx = i + 1;
         int current = friendIdx;
         while (true) {
-            current = gifts[current - 1];  // Get the friend who gave the gift
-            if (current == friendIdx) {  // If friend gave the gift to themselves
+            current = gifts[current - 1]; 
+            if (current == friendIdx) {  
                 givers[friendIdx - 1] = friendIdx;
                 break;
             } else if (gifts[current - 1] == friendIdx) {  // If friend found who gave the gift
