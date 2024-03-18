d9dc9a96eff816a46e4d4b26dacb97babaee7169
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-18 22:13:41 +0530

    even odds

--- codeforces/evenodds.cpp
+++ codeforces/evenodds.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    long long n, k;
+    int n, k;
     cin >> n >> k;
 
-    long long odd_count;
+    int odd_count;
     if (n % 2 == 1) {
         odd_count = (n + 1) / 2;
     } else {
         odd_count = n / 2;
     }
 
-    long long result;
+    int result;
     if (k <= odd_count) {
         result = 2 * k - 1;
     } else {
