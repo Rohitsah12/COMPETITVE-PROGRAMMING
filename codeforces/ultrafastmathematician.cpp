2a1a2d3abd96b7324a4a8f27022bba9fb30a2a69
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-19 21:37:24 +0530

    ultrafast mathematician

--- codeforces/ultrafastmathematician.cpp
+++ codeforces/ultrafastmathematician.cpp
@@ -1,19 +1,13 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    // Read the input numbers
     string num1, num2;
     cin >> num1 >> num2;
-
-    // Initialize an empty string to store the answer
     string result = "";
-
-    // Iterate through the digits of the numbers and compare them
     for (int i = 0; i < num1.length(); ++i) {
-        // If the digits are different, append '1' to the result
+
         if (num1[i] != num2[i]) {
             result += '1';
         }
