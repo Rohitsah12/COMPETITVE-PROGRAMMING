03d91e47a5f9ed9aa4f8beb1dc0a991887d670fc
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-20 20:39:02 +0530

    hq9+

---
+++ codeforces/hq9+.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string will_produce_output(string program) {
+    for (char ch : program) {
+        if (ch == 'H' || ch == 'Q' || ch == '9') {
+            return "YES";
+        }
+    }
+    return "NO";
+}
+
+int main() {
+    string program;
+    cin >> program;
+
+    string output = will_produce_output(program);
+    cout << output << endl;
+
+    return 0;
+}
