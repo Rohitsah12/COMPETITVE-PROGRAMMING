1ed551a33d122b2ab54877e9b31fbb96bbfc3411
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-03-31 19:27:57 +0530

    sequence game

---
+++ codeforces/sequence_game.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+      int q;
+    cin >> q;
+    while (q--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            if (i && a.back() > x) {
+                a.push_back(1);
+            }
+            a.push_back(x);
+        }
+        cout << a.size() << "\n";
+        for (int el : a)
+            cout << el << " ";
+        cout << "\n";
+    }
+    return 0;
+}
