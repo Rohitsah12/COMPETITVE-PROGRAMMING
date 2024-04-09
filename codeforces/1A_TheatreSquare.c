e5acd7ff83e05c39470a0d1964bbfe61c005cb33
Rohit Kumar Sah <148933953+Rohitsah12@users.noreply.github.com>
2024-04-09 11:49:17 +0530

    Update 1A_TheatreSquare.c

--- codeforces/1A_TheatreSquare.c
+++ codeforces/1A_TheatreSquare.c
@@ -4,8 +4,6 @@
 int main() {
     int n, m, a;
     scanf("%d %d %d", &n, &m, &a);
-
-
     long long flagstones_needed = (long long) ceil((double) n / a) * (long long) ceil((double) m / a);
 
     printf("%lld\n", flagstones_needed);
