6f9cf3954228b50354bbc51dcf10a91eeb0764d6
Rohit Kumar Sah <148933953+Rohitsah12@users.noreply.github.com>
2024-04-09 11:50:23 +0530

    Update 96AFootball.cpp

--- codeforces/96AFootball.cpp
+++ codeforces/96AFootball.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main() {
     string situation;
     cin >> situation;
-
     bool dangerous = false;
     int count = 1;
-
     for (int i = 1; i < situation.length(); ++i) {
         if (situation[i] == situation[i - 1]) {
             count++;
