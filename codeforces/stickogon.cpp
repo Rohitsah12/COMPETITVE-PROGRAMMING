3983cbef959d845850c786879f96a0cc064ccc78
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-21 20:41:00 +0530

    updte stickogon

--- codeforces/stickogon.cpp
+++ codeforces/stickogon.cpp
@@ -10,15 +10,14 @@ int main() {
         vector<int> array(n);
         unordered_map<int, int> freq;
 
-        // Input stick lengths and count their occurrences
         for (int i = 0; i < n; i++) {
             cin >> array[i];
             freq[array[i]]++;
         }
 
         int count_polygon = 0;
-        for (auto it = freq.begin(); it != freq.end(); it++) {
-            if (it->second >= 3) { // If a stick length occurs at least 3 times
+        for (const auto& p : freq) {
+            if (p.second >= 3) { 
                 count_polygon++;
             }
         }
