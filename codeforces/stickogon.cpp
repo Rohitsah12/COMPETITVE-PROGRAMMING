1632018e91136f011114f130c6c6811ace20d9dc
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-22 15:10:24 +0530

    leetcode 3114

--- codeforces/stickogon.cpp
+++ codeforces/stickogon.cpp
@@ -1,28 +1,37 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int maxPolygons(vector<int>& sticks) {
+    sort(sticks.begin(), sticks.end());
+    int count = 0;
+    int n = sticks.size();
+    for (int i = n - 1; i >= 0; i--) {
+        int sides = sticks[i];
+        int sticks_needed = sides ;
+        if (i - sticks_needed >= 0) {
+            count++;
+            sticks.erase(sticks.begin() + i - sticks_needed, sticks.begin() + i + 1);
+            i = i - sticks_needed;
+        }
+    }
+
+    return count-1;
+}
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
+    int testcases;
+    cin >> testcases;
+
+    while (testcases--) {
         int n;
         cin >> n;
-        vector<int> array(n);
-        unordered_map<int, int> freq;
-
+        vector<int> sticks(n);
         for (int i = 0; i < n; i++) {
-            cin >> array[i];
-            freq[array[i]]++;
+            cin >> sticks[i];
         }
-
-        int count_polygon = 0;
-        for (const auto& p : freq) {
-            if (p.second >= 3) { 
-                count_polygon++;
-            }
-        }
-
-        cout << count_polygon << endl;
+        cout << maxPolygons(sticks) << endl;
     }
+
     return 0;
 }
