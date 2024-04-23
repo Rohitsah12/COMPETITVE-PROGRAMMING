84878eea17137c1b30fb2fe72b31b95ab05a17de
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-23 22:55:40 +0530

    matrix multiplication

--- PracticeInClass/matrixmultiplicationExponentiation.cpp
+++ PracticeInClass/matrixmultiplicationExponentiation.cpp
@@ -22,3 +22,17 @@ Matrix matrix_power_smart(const Matrix& A, int x) {
     }
     return result;
 }
+Matrix matrix_power_final(const Matrix& A, int x) {
+    int n = A.size();
+    Matrix result(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i)
+        result[i][i] = 1;  
+
+    while (x > 0) {
+        if (x % 2 == 1)
+            result = matrix_multiply(result, A);
+        A = matrix_multiply(A, A);
+        x /= 2;
+    }
+    return result;
+}
