aa5342f7fa26eea9abc47f2d50746adb47e85d2f
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-04-27 20:23:14 +0530

    update

--- codeforces/georgeandaccomodation.py
+++ codeforces/georgeandaccomodation.py
@@ -1,6 +1,6 @@
 n = int(input())  # Number of rooms
 count = 0  # Initialize the count of rooms with enough space
-
+#rohit kumar sah
 for _ in range(n):
     p, q = map(int, input().split())  # Number of people already in the room, room's capacity
     if q - p >= 2:  # Check if there's enough space for George and Alex
