ba2788769dbd7d2e9da19cd162f5dd21d0c5e47a
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-09 15:32:47 +0530

    linked list

--- classDSA/linkedlist/doublylinkedlist.py
+++ classDSA/linkedlist/doublylinkedlist.py
@@ -45,6 +45,44 @@ class DLL:
             l.append(str(trav.data))
             trav=trav.next
         return '<----->'.join(l)
+    
+    def addfirst(self,data):
+        newNode=Node(data)
+        if self.isempty():
+            self.__head=newNode
+            self.__tail=newNode
+        else:
+            newNode.next=self.__head
+            self.__head.prev=newNode
+            self.__head=newNode
+        self.__size+=1
+    
+    def addAt(self,index,data):
+        if index<0 or index>self.__size:
+            raise Exception("Index out of bond")
+        if index==0:
+            return addfirst(data)
+        elif index==self.__tail:
+            return append(data)
+        else:
+            id=0
+            trav=self.__head
+            while(id!=index-1):
+                id+=1
+                trav=trav.next
+            newNode=Node(data,trav,trav.next)
+            trav.next=newNode
+            newNode.next.prev=newNode
+            self.__size+=1
+    def removefirst(self):
+        if(self.isempty()):
+            raise Exception("Size is 0")
+        else:
+            temp=self.__head
+            self.__head=self.__head.next
+            self.__head.prev=None
+            self.__head
+
 list=DLL()
 print(list.size())
 print(list.isempty())
@@ -53,3 +91,10 @@ list.append(6)
 list.append(7)
 list.append(45)
 print(list)
+print(list.size())
+list.addfirst(10)
+print(list)
+print(list.size())
+list.addAt(2,25)
+print(list)
+print(list.size())
