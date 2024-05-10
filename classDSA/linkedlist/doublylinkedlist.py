8b54b101b5c901c0d6ccf80b5b2e704ea2ce0d8d
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-10 16:17:08 +0530

    add first

--- classDSA/linkedlist/doublylinkedlist.py
+++ classDSA/linkedlist/doublylinkedlist.py
@@ -14,6 +14,40 @@ class Doublylinkedlist:
     
     def __len__(self):
         return self.__size
+    def clear(self):
+        return None
+    def size(self):
+        return self.__size
+    def isempty(self):
+        return self.__size==0
+    def append(self,elem):
+        if self.isempty():
+            self.__head=self.__tail=Node(elem,None,None)
+        else:
+            self.__tail.next=Node(elem,self.__tail,None)
+            self.__tail=self.__tail.next
+        self.__size+=1
+    def addlast(self,elem):
+        self.append(elem)
+    def addfirst(self,elem):
+        if self.isempty():
+            self.__head=self.__tail=Node(elem,None,None)
+        else:
+            self.__head.prev=Node(elem,None,self.__head)
+            self.__head=self.__head.prev
+        self.__size+=1
+        
     
 l=Doublylinkedlist()
 print(len(l))
+print(l.size())
+print(l.isempty())
+l.append(5)
+l.append(10)
+print(l.isempty())
+print(l.size())
+l.addlast(25)
+print(l.isempty())
+print(l.size())
+l.addfirst(10)
+print(l.size())
