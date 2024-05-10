fee3ebba3f25fdb9bf19cfd2d98fc15ceb5aa103
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-10 15:45:44 +0530

    doubly linked list length

--- classDSA/linkedlist/doublylinkedlist.py
+++ classDSA/linkedlist/doublylinkedlist.py
@@ -5,3 +5,15 @@ class Node:
         self.next=next
         def __str__(self):
             return str(self.data)
+
+class Doublylinkedlist:
+    def __init__(self):
+        self.__size=0
+        self.__head=None
+        self.__tail=None
+    
+    def __len__(self):
+        return self.__size
+    
+l=Doublylinkedlist()
+print(len(l))
