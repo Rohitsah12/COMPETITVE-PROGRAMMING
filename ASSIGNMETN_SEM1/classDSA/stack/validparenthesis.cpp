a0b498c45b6fa942cf27ccfac12a14055763770c
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-21 13:06:17 +0530

    valid parenthesis

---
+++ ASSIGNMETN_SEM1/classDSA/stack/validparenthesis.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char c=st.top();
+                st.pop();
+                if((s[i]=='}'&&c=='{')||(s[i]==')'&&c=='(')||(s[i]==']'&&c=='['));
+                else return false;
+            }
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
