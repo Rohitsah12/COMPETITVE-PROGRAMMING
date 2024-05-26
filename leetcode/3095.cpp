1142f1d87a128fdca63e66c3e54a4a2ce6d90c85
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-26 23:06:35 +0530

    push

--- leetcode/3095.cpp
+++ leetcode/3095.cpp
@@ -3,7 +3,6 @@ public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
     int minLen = INT_MAX;
-
     for (int start = 0; start < n; ++start) {
         int curOr = 0;
         for (int end = start; end < n; ++end) {
