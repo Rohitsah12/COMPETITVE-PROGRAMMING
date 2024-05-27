35f37fa82b21f4d8373f0189d431e7723bc8537a
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-27 12:48:03 +0530

    how many divisors

---
+++ codeforces/howmanydivisors.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int cnt=0;
+    for(int i=a;i<=b;i++){
+        if(!(c%i)) cnt++;
+    }
+    cout<<cnt;
+}
