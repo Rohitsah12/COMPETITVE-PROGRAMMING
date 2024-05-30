e9c37b471b8ae1cab2e20b8a3f42b4d7ef44ace9
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-30 20:53:34 +0530

    push

--- PracticeInClass/codesnippet.cpp
+++ PracticeInClass/codesnippet.cpp
@@ -6,47 +6,8 @@ typedef long long int ll;
 const int inf = 1e9 + 10;
 const int M = 1e9 + 7;
 using namespace std;
-bool isSorted(string& s) {
-    for (size_t i = 1; i < s.size(); ++i) {
-        if (s[i] < s[i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
 void solve(){
- int n;
-        string s;
-        cin >> n >> s;
 
-        string numbers, chaar;
-        bool flg = true;
-        bool letter = false;
-
-        for (char ch : s) {
-            if (isdigit(ch)) {
-                if (letter) {
-                    flg = false; 
-                    break;
-                }
-                chaar += ch;
-            } else if (islower(ch)) {
-                letter = true;
-                numbers += ch;
-            }
-        }
-
-        if (flg) {
-            if (!isSorted(numbers) || !isSorted(chaar)) {
-                flg = false;
-            }
-        }
-
-        if (flg) {
-            cout << "YES" << endl;
-        } else {
-            cout << "NO" << endl;
-        }
 }
 signed main(){
     ios_base::sync_with_stdio(0);
