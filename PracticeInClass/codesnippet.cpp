46a4b6968c81a0c7a6644a7c4276b0169b017e76
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-03 19:40:00 +0530

    codesnippet

---
+++ PracticeInClass/codesnippet.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define endl "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const int inf = 1e9 + 10;
+const int M = 1e9 + 7;
+using namespace std;
+
+void solve() {
+    
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+}
