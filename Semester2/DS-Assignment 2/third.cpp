d087d86ae43635808a0b9db09b95e7399c95acec
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-08 15:12:34 +0530

    third

---
+++ Semester2/DS-Assignment 2/third.cpp
@@ -0,0 +1,43 @@
+class RandomizedSet {
+public:
+    unordered_map<int, int> ump;
+    vector<int> nums;
+
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (ump.find(val) != ump.end()) {
+            return false;
+        }
+        nums.push_back(val);
+        ump[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (ump.find(val) == ump.end()) {
+            return false;
+        }
+        int lastElement = nums.back();
+        int idx = ump[val];
+        nums[idx] = lastElement;
+        ump[lastElement] = idx;
+        nums.pop_back();
+        ump.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
