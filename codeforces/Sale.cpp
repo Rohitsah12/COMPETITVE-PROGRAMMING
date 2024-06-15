f77619511a2ea18527badfcac35bf9a0c5a728a0
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-15 19:46:24 +0530

    update

--- codeforces/Sale.cpp
+++ codeforces/Sale.cpp
@@ -1,17 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n, m;
     cin >> n >> m;
-    
     vector<int> prices(n);
     for(int i = 0; i < n; ++i) {
         cin >> prices[i];
     }
-    
     sort(prices.begin(), prices.end());
-    
     int max_earnings = 0;
     for(int i = 0; i < m; ++i) {
         if(prices[i] < 0) {
@@ -21,8 +17,6 @@ int main() {
             break;
         }
     }
-    
     cout << max_earnings << endl;
-    
     return 0;
 }
