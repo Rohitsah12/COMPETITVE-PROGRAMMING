3852960a1613da2b374d08bb762c97763abb05f1
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-17 11:44:04 +0530

    get all divisors

--- algo/basicnumbertheory.cpp
+++ algo/basicnumbertheory.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define mod 10000000007
 const int MAX_N = 100000;
+using ll=long long;
 int modu(int a){
     return (a % mod + mod) % mod;
 }
@@ -33,7 +34,7 @@ int mod_powIter(int a,int b){
 }
 
 // primality testing by trial division in o(root(n))
-bool is_prime1(long long n){
+bool is_prime_rootn(long long n){
     int ans=true;
     for(long long i=2;i*i<=n;i++){
         if(n%i==0) ans=false;
@@ -49,11 +50,88 @@ void sieve(int n){
     }
     for(int i=2;i<=n;i++){
         if(is_prime[i]){
-            for(int k=2;k*i<=n;k++){
-                is_prime[k*i]=false;
+            // for(int k=2;k*i<=n;k++){
+            //     is_prime[k*i]=false;
+            // }
+            for(int j=2*i;j<=n;j+=i){
+                is_prime[j]=false;
             }
         }
     }
+}
+//How to factorize
+//A simpler way
+vector<int> bases ,  powers;
+for(int p : is_prime){
+    int n;
+    if(n%p==0){
+        int exponent=0;
+        while(n%p==0){
+            n/=p;
+            exponent++:
+        }
+        bases.push_back(p);
+        power.push_back(exponent);
+    }
+}
+
+// better way;
+//Idea: How did we speed up primality testing
+vector<int>bases,powers;
+for(int i=0;i<primes.size() && square(is_prime[i])<=n;i++){
+    if(is_prime(n)){
+        bases.push_back(n);
+        powers.push_back(1);
+        n/=n;
+    }
+    else{
+        //do as before
+        int exponent=0;
+        while(n%p==0){
+            n/=p;
+            exponent++:
+        }
+        bases.push_back(p);
+        power.push_back(exponent);
+    }
+}
+// A simpler way
+
+vector<int> bases, powers;
+for(int i = 0; i * i <= n; i++) {
+if(n % i == 0) {
+int exponent = 0;
+while(n % i == 0) {
+n /= i;
+exponent++;
+}
+bases.push_back(i);
+powers.push_back(exponent);
+}
+}
+
+if(n>1){
+    bases.push_back(n);
+    powers.push_back(1);
+}
+int mod_div(int a,int b,int d){
+    return mod_mul(a,mod_pow(b,d-2,d));
+    //asssuming a*b doesnot overflow
+    //return (a*b)%d;
+}
+
+vector<ll> get_divisors(ll n) {
+    vector<ll> divs;
+    for (int i = 1; 1LL * i * i <= n; i++) {
+        if (n % i == 0) {
+            divs.push_back(i);
+            if (i != n / i) {
+                divs.push_back(n / i);
+            }
+        }
+    }
+    sort(divs.begin(), divs.end());
+    return divs;
 }
 int main(){
     int n;
