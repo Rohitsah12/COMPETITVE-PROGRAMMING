782f90f0cd42e998b44d8fa0134368b38a2439fa
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-20 15:39:01 +0530

    update

---
+++ codeforces/ehabandgcd.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Rohit ios_base::sync_with_stdio(false);
+#define Kumar cin.tie(NULL);
+#define Sah cout.tie(NULL);
+using namespace std;
+
+void solve(){
+    int x; cin>>x;
+    cout<<1<<" "<<x-1<<endl;
+}
+
+signed main()
+{
+    Rohit Kumar Sah
+    ll t;
+    cin >> t;
+    // ll t=1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
