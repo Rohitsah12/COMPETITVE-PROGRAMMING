1280a287b4d6973d88f004dfd9f8ffb3019f9c03
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-23 21:36:53 +0530

    update

--- codeforces/roundhouse.cpp
+++ codeforces/roundhouse.cpp
@@ -17,7 +17,6 @@
 #define Kumar cin.tie(NULL);
 #define Sah cout.tie(NULL);
 using namespace std;
-
 ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
 ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
 bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
