166cff79d49032281eb3fcb4d0edd91465ac0eef
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-06-26 23:55:13 +0530

    prefix sum

--- algo/rangequeries.cpp
+++ algo/rangequeries.cpp
@@ -6,3 +6,20 @@ vector<int> findPrefixSums(const vector<int>& a) {
     }
     return prefixSums;
 }
+
+struct SegmentSumStructure {
+    vector<int> prefixSums;
+ 
+    SegmentSumStructure(const vector<int>& a) {
+        int n = a.size();
+        prefixSums.assign(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefixSums[i + 1] = prefixSums[i] + a[i];
+        }
+    }
+ 
+    int findSum(int left, int right) { // [left; right)
+        return prefixSums[right] - prefixSums[left];
+    }
+ 
+};
