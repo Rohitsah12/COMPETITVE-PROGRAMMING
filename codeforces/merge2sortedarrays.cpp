f14b391ab7cc8cb9188683891d3f61c280f905dc
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-04 21:39:43 +0530

    merging

--- codeforces/merge2sortedarrays.cpp
+++ codeforces/merge2sortedarrays.cpp
@@ -18,31 +18,48 @@
 #define Sah cout.tie(NULL);
 using namespace std;
 
-ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
-ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
-bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
-bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
-void printarr(ll arr[], ll n){fl(i,n) cout << arr[i] << " ";cout << "\n";}
-string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
-ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
-bool isPrime(int n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
-bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
-bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
-ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;a = (a * 2) % mod;}return res;}
-ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
 
-void solve(){
+void solve() {
+    ll n,m;
+    cin>>n>>m;
+    ll x=0;ll y=0;
+    vector<ll>a(n);
+    vector<ll>b(m);
+    vector<ll>c(n+m);
+    fl(i,n) cin>>a[i];
+    fl(i,m) cin>>b[i];
+    while(x<n && y<m){
+        if(a[x]<b[y]){
+            c[x+y]=a[x];
+            x++;
+        }
+        else{
+            c[x+y]=b[y];
+            y++;
+        }
+    }
+    while(x<n){
+        c[x+y]=a[x];
+        x++;
+    }
+    while(y<m){
+        c[x+y]=b[y];
+        y++;
+    }
+    fl(i,n+m) cout<<c[i]<<" ";
     
+
 }
 
-signed main()
-{
-    Rohit Kumar Sah
-    ll t;
-    cin >> t;
-    while(t--)
-    {
+int main() {
+   Rohit Kumar Sah
+    
+    ll t = 1;
+    // ll t;
+    // cin>>t;
+    while (t--) {
         solve();
     }
+    
     return 0;
 }
