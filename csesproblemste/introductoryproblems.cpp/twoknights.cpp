45134c380f5ee171535508323a478b83c110f0c8
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-17 20:52:36 +0530

    two knights

---
+++ csesproblemste/introductoryproblems.cpp/twoknights.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+const int MOD=1e9+7;
+const int INF=LLONG_MAX>>1;
+void solve(){
+    
+}
+signed main(){
+    // int t=1;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
